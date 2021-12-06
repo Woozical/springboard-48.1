5c600e8edc3b7c940ea9fd1ead573dcb4bca17c7
Mark <mark@example.dev>
2021-12-06 15:37:28 -0800

    two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,10 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keyArr, valArr) {
+  const obj = {};
+  for (let i = 0; i < keyArr.length; i++){
+    // If val array is guaranteed truthy values, this can instead be written as:
+    // obj[keyArr[i]] = valArr[i] || null
+    obj[keyArr[i]] = (i <= valArr.length-1) ? valArr[i] : null;
+  }
+  return obj;
 }
