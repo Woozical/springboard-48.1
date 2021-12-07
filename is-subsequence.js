ff648776714932869fc15782128ca49ba2e59243
Mark <mark@example.dev>
2021-12-06 16:22:35 -0800

    is-subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(subStr, searchStr) {
+  let validCount = 0;
+  let subPointer = 0;
+  let searchPointer = 0;
+  while (subPointer < subStr.length && searchPointer < searchStr.length){
+    if (subStr[subPointer] === searchStr[searchPointer]){
+      validCount++;
+      subPointer++;
+      searchPointer++;
+    } else {
+      searchPointer++;
+    }
+  }
+  return validCount === subStr.length;
 }
