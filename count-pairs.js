4df566c78460185ac44189a2229b35e657a9570a
Mark <mark@example.dev>
2021-12-06 16:50:16 -0800

    count-pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, target) {
+  let pairCount = 0;
+  let toExamine = new Set(arr);
+  for (let num of arr){
+    toExamine.delete(num);
+    if (toExamine.has(target - num)){
+      pairCount++;
+    }
+  }
+  return pairCount;
 }
